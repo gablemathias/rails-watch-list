450c7df6c6c526c8a39d7b7527a1c046976146d8
Gabriel Mathias <gablemathias@icloud.com>
2022-10-15 15:40:42 -0300

    adjust routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,19 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # lists routes
+
+  # get "lists", to: "lists#index"
+  # get "lists/:id", to: "lists#show"
+  # get "lists/new", to: "lists#new"
+  # get "lists/:id/edit", to: "lists#edit"
+  # delete "lists/:id", to: "lists#destroy"
+  # patch "lists/:id", to: "lists#update"
+  # post "lists", to: "lists#create"
+
+  resources :lists do
+    resources :bookmarks, only: %i[new create show destroy]
+  end
+  # get "lists/:id/bookmarks/new", to: "bookmarks#new"
 end
